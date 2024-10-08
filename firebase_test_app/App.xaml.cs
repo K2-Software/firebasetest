c3f182fa56e1c3cac1b9758416e7395ad169ff20
Krzysztof Nowak <kn@zink-software.de>
2024-10-08 19:17:21 +0200

    Add project files.

---
+++ firebase_test_app/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace firebase_test_app
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
